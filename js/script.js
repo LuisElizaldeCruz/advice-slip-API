132f16a53ebd9c6edbd282e7300cf29e9b1e8359
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-06-29 00:14:51 -0500

    se agrego identacion

--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
-
-//let campoTxto = document.querySelector(".noticia");
-
-
-
 async function getData() {
 
     try {
         let res = await fetch("https://api.adviceslip.com/advice");
+       // console.log(res);
+        if (res.ok == true){
+
         json = await res.json();
         console.log(json.slip.id);
         console.log(json.slip.advice);
 
-        /*     
-         const li = document.createElement("p");
-             li.className = "notice";
-             li.textContent=json.slip.advice;
-             li.insertAdjacentElement("afterend",campoTxto);
-             console.log(json.slip.advice);
-             */
-       let campoTxto = document.querySelector(".noticia");
-       let idNotice = document.querySelector(".idNoticia");
+        let campoTxto = document.querySelector(".noticia");
+        let idNotice = document.querySelector(".idNoticia");
+
         campoTxto.innerHTML = json.slip.advice;
-        idNotice.innerHTML= json.slip.id;
-
-
+        idNotice.innerHTML = json.slip.id;
+        }
+        else{
+            throw{estado:res.status,estadoTexto:res.statusText};
+        }
     } catch (error) {
-
+        let mensaje;
+        if(!error.statusText!==""){
+            mensaje = "ocurrio un error ";
+        }
+        console.log(mensaje,error.estado);
     } finally {
-
+        
     }
 }
 
 let boton = document.querySelector(".iconoContenedor");
-
-boton.addEventListener("click",getData);
+boton.addEventListener("click", getData);
 
 //setInterval(getData(),3000);
 
